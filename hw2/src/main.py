95d163ba2ae63b397821154914d9f3ece3410002
WangYuNeng <33870148+WangYuNeng@users.noreply.github.com>
2019-12-29 19:40:01 +0800

    final submission

--- hw2/src/main.py
+++ hw2/src/main.py
@@ -12,8 +12,6 @@ output_file = "../mpm/mpm"
 
 if __name__ == "__main__":
     client = LabClient(sys.argv[1], "B05901025")
-    with open(os.path.join(mpm_dir, "init.txt"), "r") as f:
-        best_num_group = int(f.readline())
 
     for geneation in range(1000):
         population_filename = "popu"+str(geneation).zfill(3)+".txt"
@@ -28,16 +26,8 @@ if __name__ == "__main__":
         while mgr.step():
             pass
 
-        if len(mgr.grps) >= best_num_group:
-            max_fitness = client.post_mpm(os.path.join(mpm_dir, "init.txt"), geneation)
-            copyfile(os.path.join(mpm_dir, "init.txt"), os.path.join(mpm_dir, mpm_filename))
-            print('(post init) %d: %lf' % (geneation, max_fitness))
-        else:
-            mgr.dump(os.path.join(mpm_dir, mpm_filename))
-            os.remove(os.path.join(mpm_dir, "init.txt"))
-            copyfile(os.path.join(mpm_dir, mpm_filename), os.path.join(mpm_dir, "init.txt"))
-            best_num_group = len(mgr.grps)
-            max_fitness = client.post_mpm(os.path.join(mpm_dir, mpm_filename), geneation)
-            print('(post dump) %d: %lf' % (geneation, max_fitness))
+        mgr.dump(os.path.join(mpm_dir, mpm_filename))
+        max_fitness = client.post_mpm(os.path.join(mpm_dir, mpm_filename), geneation)
+        print('(post dump) %d: %lf' % (geneation, max_fitness))
         if max_fitness == 100:
             break
